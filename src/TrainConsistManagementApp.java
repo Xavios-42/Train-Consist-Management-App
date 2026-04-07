683dce3cdc22cbe6a3883cb40fa8df843e69cc82
Xavios-42 <anushmishra2006@gmail.com>
2026-04-07 14:41:12 +0530

    Sort Passenger Bogies by Capacity (Bubble Sort)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,39 +1,33 @@
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
-        GoodsBogie rectangularBogie = new GoodsBogie("Rectangular");
-        GoodsBogie cylindricalBogie = new GoodsBogie("Cylindrical");
+        int[] capacities = {72, 56, 24, 70, 60};
 
-        rectangularBogie.assignCargo("Petroleum");
-        cylindricalBogie.assignCargo("Petroleum");
-        rectangularBogie.assignCargo("Coal");
-    }
-}
+        System.out.println("Original Capacities:");
+        display(capacities);
 
-class GoodsBogie {
-    private String shape;
-    private String cargo;
+        bubbleSort(capacities);
 
-    public GoodsBogie(String shape) {
-        this.shape = shape;
+        System.out.println("Sorted Capacities:");
+        display(capacities);
     }
 
-    public void assignCargo(String cargo) {
-        try {
-            if (shape.equals("Rectangular") && cargo.equals("Petroleum")) {
-                throw new CargoSafetyException("Unsafe assignment: Petroleum cannot be stored in Rectangular bogie.");
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
-            this.cargo = cargo;
-            System.out.println("Cargo " + cargo + " assigned safely to " + shape + " bogie.");
-        } catch (CargoSafetyException e) {
-            System.out.println("Error: " + e.getMessage());
-        } finally {
-            System.out.println("Validation completed for cargo assignment.");
         }
     }
-}
 
-class CargoSafetyException extends RuntimeException {
-    public CargoSafetyException(String message) {
-        super(message);
+    public static void display(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
     }
 }
